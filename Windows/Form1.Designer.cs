831ee0475bde3b9f833f353b793608e5f00c2464
nader <nadrmhmd057@gmail.com>
2021-10-04 05:35:51 +0200

    first

---
+++ Windows/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace Windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_inPath = new System.Windows.Forms.TextBox();
+            this.txt_OutPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(219, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(271, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_inPath
+            // 
+            this.txt_inPath.Location = new System.Drawing.Point(219, 222);
+            this.txt_inPath.Name = "txt_inPath";
+            this.txt_inPath.Size = new System.Drawing.Size(271, 20);
+            this.txt_inPath.TabIndex = 1;
+            // 
+            // txt_OutPath
+            // 
+            this.txt_OutPath.Location = new System.Drawing.Point(219, 268);
+            this.txt_OutPath.Name = "txt_OutPath";
+            this.txt_OutPath.Size = new System.Drawing.Size(271, 20);
+            this.txt_OutPath.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_OutPath);
+            this.Controls.Add(this.txt_inPath);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_inPath;
+        private System.Windows.Forms.TextBox txt_OutPath;
+    }
+}
+
---
+++ Windows/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputFilePath = txt_inPath.Text;
+            string OutputFilePath = txt_OutPath.Text;  //Please type the path only without the file name
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            try
+            {
+                ParseData(inputFilePath, OutputFilePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            sw.Stop();
+            MessageBox.Show(sw.ElapsedTicks.ToString());
+        }
+        /// <summary>
+        /// validate if file input is exist and have valid name;
+        /// </summary>
+        /// <param name="inputFilePath"></param>
+        /// <returns></returns>
+        public static bool ValidateInputFile(string inputFilePath)
+        {
+
+            FileInfo file = new FileInfo(inputFilePath); //get file info;         
+            if (!file.Exists) //check if file exist;
+                return false;
+            if (file.Name != string.Format("{0}.pu", DateTime.Now.ToString("dd-MM-yyyy"))) //check if file name is valid;
+                return false;
+            return true;
+        }
+        public static void writeinfilr(string OutputFilePath)
+        {
+
+        }
+        /// <summary>
+        /// Reading an input file and then writing it to an output file after processing the data;
+        /// </summary>
+        /// <param name="inputFilePath"></param>
+        /// <param name="OutputFilePath"></param>
+        /// 
+        public static void ParseData(string inputFilePath, string OutputFilePath)
+        {
+            if (!ValidateInputFile(inputFilePath))      //validate input file;
+                throw new Exception("The file does not exist or the file name is not in the correct format !");
+            var fileLines = File.ReadAllLines(inputFilePath);    //Read all lines of the file;
+            var first_line = fileLines[0].Split(',').ToList();   //Read the first line of the file;
+            if (first_line[0] != ("date:" + inputFilePath.Substring(inputFilePath.Length - 13, 10))) //Substring the date part from the first line and then match with The date in the file name;
+                throw new Exception("Invalid file : Date does not match !");
+            if (first_line[1] != "count:" + (fileLines.Count() - 1).ToString()) //validate that the count of lines in the file is correct;
+                throw new Exception("Invalid file : The count of lines in the file is incorrect !");
+            using (StreamWriter sw = new StreamWriter(OutputFilePath + inputFilePath.Substring(inputFilePath.Length - 13, 10) + ".ur")) //Writing in the extracted file;
+            {
+                sw.WriteLine(fileLines[0] + "\n" + "{");
+                for (int r = 1; r < fileLines.Length; r++)
+                {   //Put a line by line in an array and divide it into two parts, the first is the number and the string, and the second is the array of string;
+                    string[] All = fileLines[r].ToString().Split(new string[] { "[", "]" }, StringSplitOptions.None);
+                    string[] first_half = All[0].Split(new char[] { ',' }); //put part One{Number , string}in array;
+                    if (first_half.Count() != 4)  //Check the number of columns in all lines;
+                        throw new Exception("Invalid file : The count of columns is incorrect !");
+                    sw.WriteLine("    [");
+                    //Writing the first half {Number , string};
+                    sw.WriteLine("        age:" + first_half[0].ToString() + "," + "\n" + "        country:" + first_half[1].ToString() + "," + "\n" + "        Name:" + first_half[2].ToString());
+                    if (All[1].Contains(","))   //Find out if there is between [] more than one element {array of string};  
+                    {   //Write more than one element;
+                        string[] last_half = All[1].Split(new char[] { ',' }); //put part second{array of string}in array;
+                        sw.WriteLine("        Date:[");
+                        for (int x = 0; x < last_half.Length; x++)
+                        {
+                            sw.WriteLine("             " + last_half[x] + ",");
+                        }
+                        sw.WriteLine("        ]");
+                        sw.WriteLine("    ]");
+                    }
+                    else
+                    {   //If it is one element;
+                        sw.WriteLine("        Date:[");
+                        sw.WriteLine("            " + All[1]);
+                        sw.WriteLine("        ]");
+                        sw.WriteLine("    ],");
+                    }
+                }
+            }
+            MessageBox.Show("The data from the file has been successfully processed");
+        }
+    }
+}
